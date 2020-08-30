c11e7f275a6ba7fbe8a0c44ce593ca752cf89b3a
Matthew Burruss <burrussmatthew@gmail.com>
2020-08-30 14:16:06 -0400

    added provider to all of app so that the entire app now has access the store

--- App.tsx
+++ App.tsx
@@ -3,17 +3,16 @@ import { AsyncStorage, Button, Text, TextInput, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import config from 'config';
-
 import Login from 'screens/Login';
 import Splash from 'screens/Splash';
+import Home from 'screens/Home';
 
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import AuthReducer from 'Reducer';
+import Reducer from 'Reducer';
 
 
-const AuthContext = React.createContext({
+const AppContext = React.createContext({
   language: "EN",
   theme: "light",
   authenticated: false
@@ -21,7 +20,8 @@ const AuthContext = React.createContext({
 
 
 const Stack = createStackNavigator();
-const store = createStore(AuthReducer);
+const store = createStore(Reducer);
+
 export default function App({ navigation }) {
   useEffect(() => {
     const bootstrapAsync = async () => {
@@ -29,7 +29,6 @@ export default function App({ navigation }) {
       try {
         userToken = await AsyncStorage.getItem('userToken');
       } catch (e) {
-        console.log(e);
       }
       dispatch({ type: 'RESTORE_TOKEN', token: userToken });
     };
@@ -37,73 +36,30 @@ export default function App({ navigation }) {
     bootstrapAsync();
   }, []);
 
-  const authContext = React.useMemo(
-    () => ({
-      signIn: async (data : Object) => {
-        fetch(`${config.server}/auth/login`,{
-          method: 'POST',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-            },
-          body: JSON.stringify(data)
-        }).then(async res=>{
-          if (res.ok){
-            return res.json()
-          } else {
-            throw new Error(JSON.stringify({
-              StatusCode : res.status,
-              Message: (await res.json()).error
-            }))
-          }
-        }).then(async data=>{
-            await AsyncStorage.setItem('userToken',data.token);
-            dispatch({ type: 'SIGN_IN', token: data.token });
-          })
-          .catch(err=>{
-            console.log(err);
-          })
-      },
-      signOut: async () => {
-        let userToken = await AsyncStorage.getItem("userToken");
-        console.log(`Signing out: ${userToken}`);
-        await AsyncStorage.setItem("userToken","");
-        dispatch({ type: 'SIGN_OUT' })
-      },
-      signUp: async data => {
-        // In a production app, we need to send user data to server and get a token
-        // We will also need to handle errors if sign up failed
-        // After getting token, we need to persist the token using `AsyncStorage`
-        // In the example, we'll use a dummy token
-
-        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
-      },
-    }),
-    []
-  );
-
   return (
-    <AuthContext.Provider value={{
+    <AppContext.Provider value={{
       "language" : 'EN',
       "theme" : "light",
       "authenticated" : false,
     }}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          {state.isLoading ? (
-            <Stack.Screen name="Splash" component={Splash} />
-          ) : state.userToken == null ? (
-            <Stack.Screen name="Login" component={Login}
-              options={{
-                title: 'Sign in',
-                animationTypeForReplace: state.isSignout ? 'pop' : 'push',
-              }}
-            />
-          ) : (
-            <Stack.Screen name="Home" component={HomeScreen} />
-          )}
-        </Stack.Navigator>
-      </NavigationContainer>
-    </AuthContext.Provider>
+      <Provider store={store}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            {state.isLoading ? (
+              <Stack.Screen name="Splash" component={Splash} />
+            ) : state.userToken == null ? (
+              <Stack.Screen name="Login" component={Login}
+                options={{
+                  title: 'Sign in',
+                  animationTypeForReplace: state.isSignout ? 'pop' : 'push',
+                }}
+              />
+            ) : (
+              <Stack.Screen name="Home" component={Home} />
+            )}
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
+    </AppContext.Provider>
   );
 }
---
+++ screens/Home.tsx
@@ -0,0 +1,15 @@
+"use strict";
+
+import React from 'react';
+import {Text, View } from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Welcome to Locker!</Text>
+        </View>
+    );
+};
+
+export default Home;
+  
