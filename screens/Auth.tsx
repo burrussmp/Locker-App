32051debf484b66f448dc7d2892b0c92649113c0
Paul Sullivan <paul.h.sullivan@vanderbilt.edu>
2020-09-09 23:55:46 -0500

    updated auth

--- screens/Auth.tsx
+++ screens/Auth.tsx
@@ -20,46 +20,48 @@ import logoImagePath from 'assets/images/logo.png';
 
 const AuthScreen = (props: any) => {
   return (
-    <SafeAreaView style={styles.droidSafeArea}>
+    <View style={styles.container}>
       <ImageBackground
         source={backgroundImagePath}
         style={styles.authBackgroundImage}
       >
-        <View style={styles.centered}>
-          <Image source={logoTextImagePath} style={styles.logoText}></Image>
-          <Image source={logoImagePath}></Image>
-        </View>
-        <View style={styles.authButtonContainer}>
-          <View style={[styles.authButton, styles.whiteBackground]}>
-            <Text style={styles.authButtonText}>Sign Up</Text>
+      <SafeAreaView style={styles.droidSafeArea}>
+          <View style={styles.centered}>
+            <Image source={logoTextImagePath} style={styles.logoText}></Image>
+            <Image source={logoImagePath}></Image>
           </View>
-          <BlurView tint={'default'} intensity={100} style={styles.authButton}>
-            <View style={styles.authButton}>
-              <Text style={styles.authButtonBlurredText}>Login</Text>
+          <View style={styles.authButtonContainer}>
+            <View style={[styles.authButton, styles.whiteBackground]}>
+              <Text style={styles.authButtonText}>Sign Up</Text>
             </View>
-          </BlurView>
-        </View>
-        <View style={styles.authButtonContainer}>
-          <TouchableOpacity
-            activeOpacity={1}
-            style={[styles.authButton, styles.authButtonOverlay]}
-            onPress={() => {
-              props.navigation.navigate('Register')
-            }}>
-            <Text style={styles.authButtonBlurredText}>Sign Up</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            activeOpacity={0.5}
-            style={[styles.authButton, styles.authButtonBlurredOverlay]}
-            onPress={() => {
-              props.navigation.navigate('Login')
-            }}
-          >
-            <Text style={styles.authButtonBlurredText}>Login</Text>
-          </TouchableOpacity>
-        </View>
+            <BlurView tint={'default'} intensity={100} style={styles.authButton}>
+              <View style={styles.authButton}>
+                <Text style={styles.authButtonBlurredText}>Login</Text>
+              </View>
+            </BlurView>
+          </View>
+          <View style={styles.authButtonContainer}>
+            <TouchableOpacity
+              activeOpacity={1}
+              style={[styles.authButton, styles.authButtonOverlay]}
+              onPress={() => {
+                props.navigation.navigate('Register')
+              }}>
+              <Text style={styles.authButtonBlurredText}>Sign Up</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              activeOpacity={0.5}
+              style={[styles.authButton, styles.authButtonBlurredOverlay]}
+              onPress={() => {
+                props.navigation.navigate('Login')
+              }}
+            >
+              <Text style={styles.authButtonBlurredText}>Login</Text>
+            </TouchableOpacity>
+          </View>
+        </SafeAreaView>
       </ImageBackground>
-    </SafeAreaView>
+    </View>
   )
 }
 
