666aa06ebed7e06228b6acd8ceabcf7d75c2f81f
Matthew Burruss <burrussmatthew@gmail.com>
2021-01-24 17:19:19 -0500

    removed unneccessary things

--- api/user.tsx
+++ api/user.tsx
@@ -75,21 +75,6 @@ export type UserInfoType = T.TypeOf<typeof UserInfoType>;
 /**
  * @desc List all users
  * @return {Promise<UsersListType>} A list of all the users.
- * @success
-  ```
-[{
-    "_id": "5f34821b0c46f63b28831230",
-    "username": "userA",
-    "updated": "2020-08-12T23:58:19.944Z",
-    "created": "2020-08-12T23:58:19.944Z"
-  },
-  {
-    "_id": "5f34821c0c46f63b28831231",
-    "username": "userB",
-    "updatedAt": "2020-08-12T23:58:20.137Z",
-    "createdAt": "2020-08-12T23:58:20.137Z"
-  }]
-```
  */
 const GetAll = async (): Promise<UsersListType> => {
   const res = await utils.getRequest('/api/users');
@@ -100,27 +85,6 @@ const GetAll = async (): Promise<UsersListType> => {
  * @desc Get specific user's information. If not provided, get self.
  * @param {string | undefined} userId The user ID of a user.
  * @return {Promise<UserInfoType>} The information of a user
- * @success
-  ```
-    {
-        "about": "This is a bio",
-        "following": [{
-            "_id" : "5f6565f0c1708f4ad08477c7",
-        }],
-        "followers": [],
-        "_id": "5f6565f0c1708f4ad08477c7",
-        "cognito_username": "5627cc28-bb8f-4806-9cc9-30e2f4d042ed",
-        "username": "matthew5",
-        "first_name": "matt",
-        "last_name": "burr",
-        "createdAt": "2020-09-19T01:59:12.041Z",
-        "updatedAt": "2020-09-19T02:00:28.398Z",
-        "profile_photo": {
-            "_id": "5f65663cc1708f4ad08477c8",
-            "key": "397fec5422857e916e1fa0abeea28e32_profile_photo",
-            "mimetype": "image/jpeg"
-    }
-  ```
  */
 const GetByID = async (userId?: string): Promise<UserInfoType> => {
   const res = await utils.getRequest(`/api/users/${userId || utils.getIDAndAccessToken()._id}`);
@@ -130,31 +94,6 @@ const GetByID = async (userId?: string): Promise<UserInfoType> => {
 /**
  * @desc Delete yourself and logout.
  * @return {Promise<UserInfoType>} The info of yourself.
- * @success
-  ```
- {
-    "permissions": [
-        "post:read",
-        "post:interact",
-        "user:edit_content",
-        "user:delete",
-        "user:read"
-    ],
-    "gender": "",
-    "about": "",
-    "following": [],
-    "followers": [],
-    "_id": "5f3dcd97832746181006b1eb",
-    "username": "JohnDoe",
-    "phone_number": "000-111-2222",
-    "first_name": "John",
-    "last_name": "Doe",
-    "email": "a@mail.com",
-    "createdAt": "2020-08-20T01:10:47.626Z",
-    "updatedAt": "2020-08-20T01:10:47.626Z",
-    "__v": 0
-}
-  ```
  */
 const DeleteMe = async (): Promise<UserInfoType> => {
   const res = await utils.deleteRequest(`/api/users/${utils.getIDAndAccessToken()._id}`);
@@ -167,12 +106,6 @@ const DeleteMe = async (): Promise<UserInfoType> => {
  * @param {string} newPassword The new password.
  * @param {string} oldPassword The old password.
  * @return {Promise<{message: string}>} A success message
- * @success
-  ```
-    {
-        "message": "Successfully updated password"
-    }
-  ```
  */
 const UpdatePassword = async (newPassword: string, oldPassword: string): Promise<{message: string}> => {
   const res = await utils.putRequest(`/api/users/${utils.getIDAndAccessToken()._id}/password`, {
@@ -186,11 +119,6 @@ const UpdatePassword = async (newPassword: string, oldPassword: string): Promise
  * @desc Follow someone
  * @param {string} userId The user ID of a user.
  * @return {Promise<{message: string}>} A success message
- * @success
-  ```
-    {
-        "message": "Successfully followed someone"
-    }
   ```
  */
 const Follow = async (userId: string): Promise<{message: string}> => {
@@ -202,12 +130,6 @@ const Follow = async (userId: string): Promise<{message: string}> => {
  * @desc Unfollow someone
  * @param {string} userId The user ID of a user.
  * @return {Promise<{message: string}>} A success message
- * @success
-  ```
-    {
-        "message": "Successfully unfollowed someone"
-    }
-  ```
  */
 const Unfollow = async (userId: string): Promise<{message: string}> => {
   const res = await utils.deleteRequest(`/api/users/${userId}/follow`);
@@ -218,27 +140,6 @@ const Unfollow = async (userId: string): Promise<{message: string}> => {
  * @desc List someone's following/followers
  * @param {string} userId The user ID of a user.
  * @return {Promise<FollowingListType>} A users's list of followers/followings.
- * @success
-  ```javascript
-  {
-    "following": [
-        {
-            "_id": "5f3e184ad4df2d2ab0d5f91b",
-            "username": "new_user"
-        },
-        {
-            "_id": "5f3e184ad4dfads2ab0d5f91",
-            "username": "user2"
-        }
-    ],
-    "followers": [
-        {
-            "_id": "5f3e183dd4df2d2ab0d5f919",
-            "username": "user3"
-        },
-    ]
-  }
-```
  */
 const GetFollowing = async (userId: string): Promise<FollowingListType> => {
   const res = await utils.getRequest(`/api/users/${userId}/follow`);
